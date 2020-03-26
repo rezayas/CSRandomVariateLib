ff3f0eb2200d14bea761260bac190af6551c1b60
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2020-03-26 19:55:39 -0400

    1

--- CSRandomVariateLib/CSRandomVariateLib/Beta.cs
+++ CSRandomVariateLib/CSRandomVariateLib/Beta.cs
@@ -42,9 +42,7 @@ namespace RandomVariateLib
         {
             double critical = MathNet.Numerics.Distributions.Normal.InvCDF(0, 1, 1 - 0.05 / 2);
             double stDev = halfWidth / (critical * (max - min));
-            Console.WriteLine(stDev);
             double stMean = (mean - min) / (max - min);
-            Console.WriteLine(stMean);
             double alphaPlusBeta = stMean * (1 - stMean) / Math.Pow(stDev, 2) - 1;
             double alpha = stMean * alphaPlusBeta;
             double beta = alphaPlusBeta - alpha;
